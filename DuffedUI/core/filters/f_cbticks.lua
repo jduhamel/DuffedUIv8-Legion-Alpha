3e48f2a428fcf05868336636eb1d82effa76aaca
liquidbase <liquid.base.x@gmail.com>
2016-04-22 19:46:15 +0200

    Worldmap & Castbarticks

--- DuffedUI/core/filters/f_cbticks.lua
+++ DuffedUI/core/filters/f_cbticks.lua
@@ -8,10 +8,8 @@ local function SpellName(id)
 end
 
 D.ChannelTicks = {
-	[SpellName(10)] = 8,
 	[SpellName(689)] = 6,
 	[SpellName(755)] = 6,
-	[SpellName(1949)] = 15,
 	[SpellName(5143)] = 5,
 	[SpellName(5740)] = 4,
 	[SpellName(12051)] = 4,
@@ -20,11 +18,7 @@ D.ChannelTicks = {
 	[SpellName(47540)] = 2,
 	[SpellName(48045)] = 5,
 	[SpellName(64843)] = 4,
-	[SpellName(103103)] = 3,
-	[SpellName(106996)] = 10,
-	[SpellName(108371)] = 6,
 	[SpellName(115175)] = 9,
-	[SpellName(139139)] = 3,
 }
 
 D.HasteTicks = {
