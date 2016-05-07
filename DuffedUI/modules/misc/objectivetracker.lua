13fc9d11596da07f670c7a3db2416d14d38d2f2a
liquidbase <liquid.base.x@gmail.com>
2016-05-08 00:04:12 +0200

    Remove level display from gossipframe

--- DuffedUI/modules/misc/objectivetracker.lua
+++ DuffedUI/modules/misc/objectivetracker.lua
@@ -240,34 +240,6 @@ end)
 --[[Questlevel display]]
 local QuestLevelPatch = {}
 
-function GossipFrameUpdate_hook()
-	local buttonIndex = 1
-
-	local availableQuests = {GetGossipAvailableQuests()}
-	local numAvailableQuests = table.getn(availableQuests)
-	for i = 1, numAvailableQuests, 6 do
-		local titleButton = _G["GossipTitleButton" .. buttonIndex]
-		local title = "[" .. availableQuests[i + 1] .. "] " .. availableQuests[i]
-		local isTrivial = availableQuests[i + 2]
-		if isTrivial then titleButton:SetFormattedText(TRIVIAL_QUEST_DISPLAY, title) else titleButton:SetFormattedText(NORMAL_QUEST_DISPLAY, title) end
-		GossipResize(titleButton)
-		buttonIndex = buttonIndex + 1
-	end
-	if numAvailableQuests > 1 then buttonIndex = buttonIndex + 1 end
-
-	local activeQuests = {GetGossipActiveQuests()}
-	local numActiveQuests = table.getn(activeQuests)
-	for i = 1, numActiveQuests, 5 do
-		local titleButton = _G["GossipTitleButton" .. buttonIndex]
-		--local title = "[" .. activeQuests[i + 1] .. "] " .. activeQuests[i]
-		local isTrivial = activeQuests[i + 2]
-		if isTrivial then titleButton:SetFormattedText(TRIVIAL_QUEST_DISPLAY) else titleButton:SetFormattedText(NORMAL_QUEST_DISPLAY) end
-		GossipResize(titleButton)
-		buttonIndex = buttonIndex + 1
-	end
-end
-hooksecurefunc("GossipFrameUpdate", GossipFrameUpdate_hook)
-
 function SetBlockHeader_hook()
 	for i = 1, GetNumQuestWatches() do
 		local questID, title, questLogIndex, numObjectives, requiredMoney, isComplete, startEvent, isAutoComplete, failureTime, timeElapsed, questType, isTask, isStory, isOnMap, hasLocalPOI = GetQuestWatchInfo(i)
@@ -280,21 +252,6 @@ function SetBlockHeader_hook()
 	end
 end
 hooksecurefunc(QUEST_TRACKER_MODULE, "Update", SetBlockHeader_hook)
-
---[[function QuestLogQuests_hook(self, poiTable)
-	local numEntries, numQuests = GetNumQuestLogEntries()
-	local headerIndex = 0
-	for questLogIndex = 1, numEntries do
-		local title, level, suggestedGroup, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isStory = GetQuestLogTitle(questLogIndex)
-		if isOnMap and not isTask and not isHeader then
-			headerIndex = headerIndex + 1
-			local button = QuestLogQuests_GetTitleButton(headerIndex)
-			local newTitle = "[" .. level .. "] " .. button.Text:GetText()
-			button.Text:SetText(newTitle)
-		end
-	end
-end
-hooksecurefunc("QuestLogQuests_Update", QuestLogQuests_hook)]]--
 
 --[[Execution]]--
 local ObjFhandler = CreateFrame("Frame")
