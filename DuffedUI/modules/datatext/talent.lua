97b072575ad0a48bdca3c64b79b34f51e7601fce
djriff <riff.ibanezius@gmail.com>
2016-06-12 18:23:30 -0700

    I ended up having to put the whole talent swap function in the datatext. This rendered the ses.lua pretty uneeded. The only thing left to do is add in the item set swaps depending on talents, which I can work on if need be.

--- DuffedUI/modules/datatext/talent.lua
+++ DuffedUI/modules/datatext/talent.lua
@@ -1,7 +1,33 @@
-﻿local D, C, L = unpack(select(2, ...))
-
+local D, C, L = unpack(select(2, ...))
+--As long as blizzard doesn't go back to dual spec, this should work forever, even with new classes with no changes.
 if C["datatext"].talent and C["datatext"].talent > 0 then
-	local Stat = CreateFrame("Frame", "DuffedUIStatTalent")
+
+	local LeftClickMenu = { }
+	LeftClickMenu[1] = { text = "DuffedUI Specialization Selector", isTitle = true, notCheckable = true}
+
+	local DuffedUISpecSwap = CreateFrame("Frame", "DuffedUISpecSwap", UIParent, "UIDropDownMenuTemplate") --Setting up the menu for later for each spec regardless of class, thanks to Simca for helping out with the function.
+	DuffedUISpecSwap:SetTemplate("Transparent")
+	DuffedUISpecSwap:RegisterEvent("PLAYER_LOGIN")
+	DuffedUISpecSwap:SetScript("OnEvent", function(...)
+		local specIndex
+		for specIndex = 1, GetNumSpecializations() do
+			LeftClickMenu[specIndex + 1] = {
+				text = tostring(select(2, GetSpecializationInfo(specIndex))),
+				notCheckable = true,
+				func = (function()
+					local getSpec = GetSpecialization()
+					if getSpec and getSpec == specIndex then
+						UIErrorsFrame:AddMessage("You're already in that spec!", 1.0, 0.0, 0.0, 53, 5);
+						return
+					end
+					SetSpecialization(specIndex)
+				end)
+			}
+		end
+	end)
+	
+
+	local Stat = CreateFrame("Frame", "DuffedUIStatTalent") --The datatext
 	Stat:EnableMouse(true)
 	Stat:SetFrameStrata("BACKGROUND")
 	Stat:SetFrameLevel(3)
@@ -14,7 +40,7 @@ if C["datatext"].talent and C["datatext"].talent > 0 then
 	Text:SetFont(f, fs, ff)
 	D.DataTextPosition(C["datatext"].talent, Text)
  
-	local function Update(self)
+	local function Update(self) --The pretty part of the data text, displays the name of the spec.
 		if not GetSpecialization() then
 			Text:SetText(L["dt"]["talent"]) 
 		else
@@ -29,8 +55,9 @@ if C["datatext"].talent and C["datatext"].talent > 0 then
 	Stat:RegisterEvent("CONFIRM_TALENT_WIPE")
 	Stat:RegisterEvent("PLAYER_TALENT_UPDATE")
 	Stat:SetScript("OnEvent", Update)
-	Stat:SetScript("OnMouseDown", function()
-		local c = GetActiveSpecGroup(false,false)
-		SetActiveSpecGroup(c == 1 and 2 or 1)
-	end)
+	Stat:SetScript("OnMouseDown", function(self, btn)
+		if btn == "LeftButton" then 
+		EasyMenu(LeftClickMenu, DuffedUISpecSwap, "cursor", 0, 0, "MENU", 2) --Dropdown/popup menu for spec selection.
+		end
+    end)
 end
